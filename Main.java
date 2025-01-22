4f79c31b5e4f5db10b5c0ec88394002cfa615540
VishvijayPS <vishvijayps123@mail.com>
2025-01-22 17:23:43 +0530

    changing the main function

--- Main.java
+++ Main.java
@@ -7,15 +7,15 @@ public class Main {
         try (Scanner sc = new Scanner(System.in)) {
             Hash_char hs = new Hash_char();
             Random random = new Random();
-            char[] randomChars = {'w', 'O', '£', 'z', 'R', 'S', 'l', 'U','v', 'N', 'x', 'y', 'Q','j', 'k', 'T', 'm','@','P','$','&'};
+            char[] randomChars = {'w', 'O', 'z', 'R', 'S', 'l', 'U','v', 'N', 'x', 'y', 'Q','j', 'k', 'T', 'm','@','P','$','&'};
 
             System.out.print("Enter the sentence: ");
             String input = sc.nextLine();
 
             String hashedString = convertAndLoadToString(input, hs, randomChars, random);
 
-            System.out.println("KeyQuill hash value :");
-            System.out.println(hashedString);
+            System.out.print("KeyQuill hash value :");
+            System.out.print(hashedString);
         }
     }
 
